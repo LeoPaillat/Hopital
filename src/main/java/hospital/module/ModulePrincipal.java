407f845d93b548950e6a5fc087ccf7466f63c2a0
leo paillat <leopaillat@yhoo.fr>
2023-03-07 13:37:52 +0100

    ajout src

---
+++ src/main/java/hospital/module/ModulePrincipal.java
@@ -0,0 +1,13 @@
+package hospital.module;
+
+public class ModulePrincipal {
+//	public static void main (String[] args) {
+//	}
+	
+	public void menuPrincipal() {
+		System.out.println("----------------Menu principal :-----------------");
+		System.out.println("Identification :");
+		
+	}
+
+}
